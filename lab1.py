90c3c765c7275baf5908e185bc0f4ed3f5a7beb8
Andrew-Ponomarenko <61511780+Andrew-Ponomarenko@users.noreply.github.com>
2022-10-12 11:41:50 +0300

    замінив функціі на ручні

--- lab1.py
+++ lab1.py
@@ -1,20 +1,43 @@
-#використані бібліотеки
+#used libraries
 from numpy import *
 from matplotlib.pyplot import *
-from scipy.stats import *
 
-filename = raw_input()#бере назву файла з консолі
-f = open(filename,"r")#відкриває файл
-len = int(f.readline())#бере довжину з першого рядку файла
-datarray = []#ініціалізує масив данних
-
+filename = raw_input()
+f = open(filename,"r")#opens file
+len = int(f.readline())#takes file length from first line
+datarray = []#initialises data array
 for x in range(len):
     datarray.append(int(f.readline()))
 
-fig = hist(datarray)#малюємо гістограму
-#записуємо властивості данних в файл
-title('median='+str(median(datarray))+", mode="+str(mode(datarray).mode)+", dispersion="+str(iqr(datarray))+",\nstandard deviation="+str(std(datarray))+", quartile deviation="+str((quantile(datarray,0.75)-quantile(datarray,0.25))/2))
-#підписи
+datarray = sort(datarray)
+fig = hist(datarray)#draws histogram
+#writes data properties onto the histogram
+title('data histogram')
+#labels
 xlabel('value')
 ylabel('frequency')
-savefig("output from "+filename+".png")#зберігає файл
+savefig("output from "+filename+".png")#saves file
+#properties but more manual
+f.close()
+f = open("output from "+filename+".txt","w")
+if (len % 2) == 1:#check odd
+    f.write("Median = "+str(datarray[len/2]))
+else:
+    f.write("Median = "+str((datarray[int(len/2)]+datarray[int(len/2+1)])/2))
+largestCount = 0
+for i in range(len):#custom function for finding mode
+        count = count_nonzero(datarray==datarray[i])
+        if(count > largestCount):
+            largestCount = count
+            valuePosition = i
+        i += count
+f.write("\nmode = "+str(datarray[valuePosition]))
+f.write("\ndispersion = "+str(float(datarray.max()-datarray.min())/float(datarray.max()+datarray.min())))
+devsum = 0.0#initialises as float
+for i in range(len):
+    devsum = devsum + (float(datarray[i])-(float(datarray.sum())/len))**2#half of the standard deviation formula
+f.write("\nstandard deviation = "+str(sqrt(devsum/(len-1))))#second half
+def cusquantile(cqnda,cloc):#custom function for finding quantiles
+    return cqnda[int(floor(len*cloc))]+(cqnda[int(ceil(len*cloc))]-cqnda[int(floor(len*cloc))])*((len*cloc) % 1)
+f.write("\nquartile deviation = "+str((cusquantile(datarray,0.75)-cusquantile(datarray,0.25))/2))
+f.close()
