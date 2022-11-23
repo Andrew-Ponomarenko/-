1c7ce4e20aa273b56afce49f5118f1f269f59fbe
Andrew-Ponomarenko <61511780+Andrew-Ponomarenko@users.noreply.github.com>
2022-11-23 09:23:07 +0200

    Add files via upload

--- lab1.py
+++ lab1.py
@@ -23,12 +23,12 @@ f = open("output from "+filename+".txt","w")
 if (len % 2) == 1:#check odd
     f.write("Median = "+str(datarray[len/2]))
 else:
-    f.write("Median = "+str((datarray[int(len/2)]+datarray[int(len/2+1)])/2))
+    f.write("Median = "+str((datarray[int(len/2)-1]+datarray[int(len/2)])/2))
 largestCount = 0
 tempfreqarray = datarray
 f.write("\ntable of frequencies:\n")
 j=0
-for i in range(len):
+for i in range(len):#function for printing freq
         if j == 0:
             f.write(str([tempfreqarray[i],count_nonzero(tempfreqarray==tempfreqarray[i])])+"\n")
             j = count_nonzero(tempfreqarray==tempfreqarray[i])
@@ -36,24 +36,24 @@ for i in range(len):
 f.write("\ntable of cumulative frequencies:\n")
 j=0
 s=0
-for i in range(len):
+for i in range(len):#function for printing cumfreq
         if j == 0:
             s = count_nonzero(tempfreqarray==tempfreqarray[i]) + s
             f.write(str([tempfreqarray[i],s])+"\n")
             j = count_nonzero(tempfreqarray==tempfreqarray[i])
         j = j - 1
-for i in range(len):#custom function for finding mode
+for i in range(len):#function for finding mode
         count = count_nonzero(datarray==datarray[i])
         if(count > largestCount):
             largestCount = count
             valuePosition = i
         i += count
 f.write("\nmode = "+str(datarray[valuePosition]))
-f.write("\ndispersion = "+str(float(datarray.max()-datarray.min())/float(datarray.max()+datarray.min())))
 devsum = 0.0#initialises as float
 for i in range(len):
     devsum = devsum + (float(datarray[i])-(float(datarray.sum())/len))**2#half of the standard deviation formula
 f.write("\nstandard deviation = "+str(sqrt(devsum/(len-1))))#second half
+f.write("\nvariation = "+str(devsum/(len-1)))#variation
 def cusquantile(cqnda,cloc):#custom function for finding quantiles
     return cqnda[int(floor(len*cloc))]+(cqnda[int(ceil(len*cloc))]-cqnda[int(floor(len*cloc))])*((len*cloc) % 1)
 f.write("\nquartile deviation = "+str((cusquantile(datarray,0.75)-cusquantile(datarray,0.25))/2))
